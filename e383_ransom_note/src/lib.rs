f1af35e08f2843d947b23737c028111b532d905b
Alex Shore <as2970@york.ac.uk>
2023-03-27 18:54:07 +0100

    383 ransom note

---
+++ e383_ransom_note/src/lib.rs
@@ -0,0 +1,31 @@
+#[allow(dead_code)]
+struct Solution;
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+        for char in ransom_note.chars() {
+            if ransom_note.matches(char).count() > magazine.matches(char).count() {
+                return false;
+            };
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::can_construct("a".to_string(), "b".to_string()), false);
+    }
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::can_construct("aa".to_string(), "ab".to_string()), false);
+    }
+    #[test]
+    fn case_3() {
+        assert_eq!(Solution::can_construct("aa".to_string(), "aab".to_string()), true);
+    }
+}
