a09a737a91cf29a39e763573d8ea6ab27513290d
Alex Shore <as2970@york.ac.uk>
2023-03-27 21:07:53 +0100

    refinement

--- e383_ransom_note/src/lib.rs
+++ e383_ransom_note/src/lib.rs
@@ -2,7 +2,34 @@
 struct Solution;
 
 impl Solution {
-    pub fn can_construct(ransom_note: String, magazine: String) -> bool {
+    pub fn can_construct(ransom_note: String, mut magazine: String) -> bool {
+        for c in ransom_note.chars() {
+            if let Some(index) = magazine.find(c) {
+                magazine.remove(index);
+            } else {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn can_construct_old2(ransom_note: String, magazine: String) -> bool {
+        let mut dict = std::collections::HashMap::new();
+
+        for c in magazine.chars() {
+            dict.entry(c).and_modify(|count| *count += 1).or_insert(1);
+        }
+
+        for c in ransom_note.chars() {
+            match dict.get_mut(&c) {
+                Some(n) if *n > 0 => *n -= 1,
+                _ => return false,
+            }
+        }
+        true
+    }
+
+    pub fn can_construct_old(ransom_note: String, magazine: String) -> bool {
         for char in ransom_note.chars() {
             if ransom_note.matches(char).count() > magazine.matches(char).count() {
                 return false;
