e4125ce368e08b4127c9ad00744f05bba24d733d
Alex Shore <as2970@york.ac.uk>
2023-03-31 18:45:09 +0100

    refactored

--- easy/e20_valid_parenthesis/src/lib.rs
+++ easy/e20_valid_parenthesis/src/lib.rs
@@ -4,17 +4,21 @@ struct Solution;
 impl Solution {
     pub fn is_valid(s: String) -> bool {
         let mut stack: Vec<char> = vec![];
-        for char in s.chars() {
-            match char {
-                '(' | '{' | '[' => stack.push(char),
+        let map = |ch| match ch {
+            ')' => '(',
+            '}' => '{',
+            ']' => '[',
+            _ => unreachable!(),
+        };
+
+        for ch in s.chars() {
+            match ch {
+                '(' | '{' | '[' => stack.push(ch),
                 ')' | '}' | ']' if stack.is_empty() => return false,
-                ')' if *stack.last().unwrap() == '(' => stack.truncate(stack.len() - 1),
-                '}' if *stack.last().unwrap() == '{' => stack.truncate(stack.len() - 1),
-                ']' if *stack.last().unwrap() == '[' => stack.truncate(stack.len() - 1),
+                ')' | '}' | ']' if *stack.last().unwrap() == map(ch) => stack.truncate(stack.len() - 1),
                 _ => return false,
             };
         }
-
         stack.is_empty()
     }
 }
